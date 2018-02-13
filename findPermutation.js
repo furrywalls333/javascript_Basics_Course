24dcc6c2067944310383c314559ede0d00e02b35
furrywalls333 <jbushey3@gmail.com>
2018-02-12 19:36:22 -0500

    Fix permutation function to not be dumb and wayyyyyyy over thought

--- findPermutation.js
+++ findPermutation.js
@@ -1,22 +1,5 @@
 var findPermutation = function(str1, str2) {
-    var strArray1 = str1.split('').sort();
-    var strArray2 = str2.split('').sort();
-    var matchArray = [];
-
-    for(var i = 0; i < strArray1.length; i++) {
-        if (strArray1[i] != strArray2[i])
-            matchArray.push(false);
-        else
-            matchArray.push(true);
-    }
-
-    return matchArray.reduce(function(acc, cur) {
-        if (cur == false)
-            return cur;
-        else
-            return acc;
-    })
-
+    return str1.split('').sort().join('') == str2.split('').sort().join(''); 
 };
 
-console.log(findPermutation('hello','lhleo'));
+console.log(findPermutation('hello','lhlmo'));
